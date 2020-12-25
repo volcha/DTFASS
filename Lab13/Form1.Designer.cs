b3ba48029a8fb8c578daa4d348b8596c320ffc23
volcha <57828516+volcha@users.noreply.github.com>
2020-12-26 01:26:11 +0300

    Chain of responsibility

---
+++ Lab13/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Lab13 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(54, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 97);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(4, 73);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(43, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "???";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.Click += new System.EventHandler(this.radioButton4_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(98, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "к стоматологу";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(79, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "к окулисту";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "к хирургу";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Направление от терапевта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 200);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chain of responsibility";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Lab13/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab13 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void radioButton1_Click(object sender, EventArgs e) {
+            Receiver receiver = new Receiver(true, true, true);
+            Handler surgeonHandler = new SurgeonHandler();
+            Handler oculistHadler = new OculistHandler();
+            Handler dentistHandler = new DentistHandler();
+            surgeonHandler.Successor = oculistHadler;
+            oculistHadler.Successor = dentistHandler;
+            string str = "";
+            surgeonHandler.Handle(receiver, str);
+        }
+        private void radioButton2_Click(object sender, EventArgs e) {
+            Receiver receiver = new Receiver(false, true, true);
+            Handler surgeonHandler = new SurgeonHandler();
+            Handler oculistHadler = new OculistHandler();
+            Handler dentistHandler = new DentistHandler();
+            surgeonHandler.Successor = oculistHadler;
+            oculistHadler.Successor = dentistHandler;
+            string str = "";
+            surgeonHandler.Handle(receiver, str);
+        }
+        private void radioButton3_Click(object sender, EventArgs e) {
+            Receiver receiver = new Receiver(false, false, true);
+            Handler surgeonHandler = new SurgeonHandler();
+            Handler oculistHadler = new OculistHandler();
+            Handler dentistHandler = new DentistHandler();
+            surgeonHandler.Successor = oculistHadler;
+            oculistHadler.Successor = dentistHandler;
+            string str = "";
+            surgeonHandler.Handle(receiver, str);
+        }
+        private void radioButton4_Click(object sender, EventArgs e) {
+            Receiver receiver = new Receiver(false, false, false);
+            Handler surgeonHandler = new SurgeonHandler();
+            Handler oculistHadler = new OculistHandler();
+            Handler dentistHandler = new DentistHandler();
+            surgeonHandler.Successor = oculistHadler;
+            oculistHadler.Successor = dentistHandler;
+            string str = "";
+            surgeonHandler.Handle(receiver, str);
+        }
+    }
+    class Receiver {
+        public bool surgeon { get; set; }
+        public bool oculist { get; set; }
+        public bool dentist { get; set; }
+        public Receiver(bool sr, bool oc, bool de) {
+            surgeon = sr;
+            oculist = oc;
+            dentist = de;
+        }
+    }
+    abstract class Handler {
+        public Handler Successor { get; set; }
+        public abstract void Handle(Receiver receiver, string str);
+    }
+    class SurgeonHandler : Handler {
+        public override void Handle(Receiver receiver, string str) {
+            if (receiver.surgeon == true) {
+                str += "Вам нужно к хирургу";
+                MessageBox.Show(str);
+            }
+            else if (Successor != null) {
+                str += "Вам не нужно к хирургу \n";
+                Successor.Handle(receiver, str);
+            }
+        }
+    }
+    class OculistHandler : Handler {
+        public override void Handle(Receiver receiver, string str) {
+            if (receiver.oculist == true) {
+                str += "Вам нужно к окулисту";
+                MessageBox.Show(str);
+            }
+            else if (Successor != null) {
+                str += "Вам не нужно к окулисту \n";
+                Successor.Handle(receiver, str);
+            }
+        }
+    }
+    class DentistHandler : Handler {
+        public override void Handle(Receiver receiver, string str) {
+            if (receiver.dentist == true) {
+                str += "Вам нужно к стомоматологу";
+                MessageBox.Show(str);
+            }
+            else if (Successor != null) {
+                Successor.Handle(receiver, str);
+            }
+            else {
+                str += "Вам не нужно к окулисту \n";
+                str += "Вы здоровы!";
+                MessageBox.Show(str);
+            }
+        }
+    }
+}
