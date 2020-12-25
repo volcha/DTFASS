542a2747db1869f00079a3830ec0be16c394427c
volcha <57828516+volcha@users.noreply.github.com>
2020-12-25 21:58:49 +0300

    Abstract Factory

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Lab12 {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.carToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.motorcycleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.carToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.motorcycleToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(284, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carToolStripMenuItem,
+            this.motorcycleToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(53, 22);
+            this.toolStripDropDownButton1.Text = "Ferrari";
+            // 
+            // carToolStripMenuItem
+            // 
+            this.carToolStripMenuItem.Name = "carToolStripMenuItem";
+            this.carToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.carToolStripMenuItem.Text = "Car";
+            this.carToolStripMenuItem.Click += new System.EventHandler(this.carToolStripMenuItem_Click);
+            // 
+            // motorcycleToolStripMenuItem
+            // 
+            this.motorcycleToolStripMenuItem.Name = "motorcycleToolStripMenuItem";
+            this.motorcycleToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.motorcycleToolStripMenuItem.Text = "Motorcycle";
+            this.motorcycleToolStripMenuItem.Click += new System.EventHandler(this.motorcycleToolStripMenuItem_Click);
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.carToolStripMenuItem1,
+            this.motorcycleToolStripMenuItem1});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(88, 22);
+            this.toolStripDropDownButton2.Text = "Lamborghini";
+            // 
+            // carToolStripMenuItem1
+            // 
+            this.carToolStripMenuItem1.Name = "carToolStripMenuItem1";
+            this.carToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.carToolStripMenuItem1.Text = "Car";
+            this.carToolStripMenuItem1.Click += new System.EventHandler(this.carToolStripMenuItem1_Click);
+            // 
+            // motorcycleToolStripMenuItem1
+            // 
+            this.motorcycleToolStripMenuItem1.Name = "motorcycleToolStripMenuItem1";
+            this.motorcycleToolStripMenuItem1.Size = new System.Drawing.Size(134, 22);
+            this.motorcycleToolStripMenuItem1.Text = "Motorcycle";
+            this.motorcycleToolStripMenuItem1.Click += new System.EventHandler(this.motorcycleToolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите,что вы хотите купить";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 188);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Abstract Factory";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem carToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem motorcycleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem carToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem motorcycleToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Lab12/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void carToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (sender is ToolStripMenuItem) {
+                Client fer = new Client(new Ferrari());
+                MessageBox.Show(fer.CarTime());
+            }
+        }
+        private void motorcycleToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (sender is ToolStripMenuItem) {
+                Client fer = new Client(new Ferrari());
+                MessageBox.Show(fer.MotoTime());
+            }
+        }
+        private void carToolStripMenuItem1_Click(object sender, EventArgs e) {
+            if (sender is ToolStripMenuItem) {
+                Client lam = new Client(new Lamborghini());
+                MessageBox.Show(lam.CarTime());
+            }
+        }
+        private void motorcycleToolStripMenuItem1_Click(object sender, EventArgs e) {
+            if (sender is ToolStripMenuItem) {
+                Client lam = new Client(new Lamborghini());
+                MessageBox.Show(lam.MotoTime());
+            }
+        }
+    }
+    abstract class Car {
+        public abstract string Time();
+    }
+    abstract class Motorcycle {
+        public abstract string Time();
+    }
+    class LaFerrari : Car {
+        static Random rnd = new Random();
+        int t = rnd.Next(15, 90);
+        public override string Time() {
+            string str = "Вы заказали машину модели LaFerrari. Время доставки ";
+            str += t.ToString();
+            str += " дней";
+            return str;
+        }
+    }
+    class Sian : Car {
+        static Random rnd = new Random();
+        int t = rnd.Next(15, 90);
+        public override string Time() {
+            string str = "Вы заказали машину модели Sian. Время доставки ";
+            str += t.ToString();
+            str += " дней";
+            return str;
+        }
+    }
+    class MotoFer : Motorcycle{
+        static Random rnd = new Random();
+        int t = rnd.Next(15, 90);
+        public override string Time() {
+            string str = "Вы заказали мотоцикл марки Ferrari. Время доставки ";
+            str += t.ToString();
+            str += " дней";
+            return str;
+        }
+    }
+    class Caramelo : Motorcycle {
+        static Random rnd = new Random();
+        int t = rnd.Next(15, 90);
+        public override string Time() {
+            string str = "Вы заказали мотоцикл модели Caramelo. Время доставки ";
+            str += t.ToString();
+            str += " дней";
+            return str;
+        }
+    }
+    abstract class Factory {
+        public abstract Car createCar();
+        public abstract Motorcycle createMotorcycle();
+    }
+    class Ferrari : Factory {
+        public override Car createCar() {
+            return new LaFerrari();
+        }
+        public override Motorcycle createMotorcycle() {
+            return new MotoFer();
+        }
+    }
+    class Lamborghini : Factory {
+        public override Car createCar() {
+            return new Sian();
+        }
+        public override Motorcycle createMotorcycle() {
+            return new Caramelo();
+        }
+    }
+    class Client {
+        private Car car;
+        private Motorcycle motorcycle;
+        public Client(Factory factory) {
+            car = factory.createCar();
+            motorcycle = factory.createMotorcycle();
+        }
+        public string CarTime() {
+            return car.Time();
+        }
+        public string MotoTime() {
+            return motorcycle.Time();
+        }
+    }
+}
